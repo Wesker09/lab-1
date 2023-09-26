d185e1d287a78c19afbb838476bd0b50d5ec993a
Wesker09 <144093987+Wesker09@users.noreply.github.com>
2023-09-26 20:39:40 +0300

    Add files via upload

---
+++ Новая папка (2)/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+
+	cout << "                         Прізвище : Гаркуша" << endl;
+	cout << "                             I'мя : Дмитро" << endl;
+	cout << "                             Стать: Чоловiча" << endl;
+	cout << "                   Дата народження: 2006-07-24" << endl;
+	cout << "                    Номер телефону: 068 930 03 16" << endl;
+	cout << "                    Мiсце навчання: ФПМ, ДНУ iм. Олеся Гончара	" << endl;
+	cout << "                            Группа: ПА-23-1" << endl;
+	cout << "                             Хоббi: саморозвиття" << endl;
+	cout << "\n\n";
+	cout << " -----------" << endl;
+	cout << "|           |" << endl;
+	cout << "|           |" << endl;
+	cout << "|           |" << endl;
+	cout << "|           |" << endl;
+	cout << " -----------" << endl;
+}
